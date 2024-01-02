566455897da84cdf53585ef53e12a9a39de2d634
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-02 14:14:47 -0800

    edits

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(-10000, 10000)
+
+last = abs(number) % 10
+
+if last == 0:
+    print(f"Last digit of {number} is {last} and is 0")
+elif last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
+elif last < 6:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
