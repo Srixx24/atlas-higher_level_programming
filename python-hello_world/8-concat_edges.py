ef8da8fc1f8f087aade513fbe0efdaf6e75eee0d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-02 13:07:21 -0800

    edits

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+
+part1 = str [39:-62]
+part2 = str[:6]
+
+print(f"{part1} with {part2}")
