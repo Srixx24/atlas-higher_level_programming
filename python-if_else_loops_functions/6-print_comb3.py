9a60e774ed509e75fb23b028b70fbf076f584745
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-02 18:21:37 -0800

    edits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if (i, j) != (8, 9) else "")
+        print("{}{}".format(i, j), end=", " if (i, j) != (8, 9) else "")
 print()
