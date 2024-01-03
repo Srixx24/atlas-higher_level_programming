f315fc45ba894920e978951ce6723246030dc6ff
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-03 07:00:28 -0800

    edits

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    uppers = ""
     for char in str:
-        print(char(ord) & 32), end='')
-    print()
+        uppers += "{}".format(chr(ord(char) & ~32))
+    print(uppers)
