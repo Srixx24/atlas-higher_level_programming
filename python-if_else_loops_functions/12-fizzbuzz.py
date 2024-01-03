d9b3b6842ed36c19775df59e033f740329e42f3b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-03 08:46:58 -0800

    edits

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -9,4 +9,4 @@ def fizzbuzz():
         elif i % 5 == 0:
             print("Buzz ", end="")
         else:
-            print("{:d}".format(i), end="")
+            print("{:d} ".format(i), end="")
