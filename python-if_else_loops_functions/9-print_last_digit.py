8ff4b5ac536bf7fa71684611b4cfe0abf83bfba8
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-03 08:03:39 -0800

    edits

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last = number % 10
-    print(last)
+    if number < 0:
+        last = (-number % 10)
+    elif number >= 0:
+        last = number % 10
+        print("{:d}".format(last), end="")
     return last
