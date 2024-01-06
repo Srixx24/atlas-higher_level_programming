39eaf8bd9cb6ad0bcd586f7486443dc866c2b711
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-06 10:43:06 -0800

    edits

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -11,3 +11,6 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
+
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
