4b42f7541049eff728ea36a9c2183c6561503cf4
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-06 13:37:57 -0800

    edits

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
-
+def multiple_returns(sentence):
+    if not sentence:
+        sentece = None
+    if sentence:
+        lenofsen = len(sentence)
+    else:
+        lenofsen = 0
+    return (lenofsen)
