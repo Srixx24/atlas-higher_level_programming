9ddbfc24eebc3e12c7315d4fdab313906ac04db0
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-06 13:11:15 -0800

    edits

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=" ")
+            print("{:d}".format(num))
         print()
