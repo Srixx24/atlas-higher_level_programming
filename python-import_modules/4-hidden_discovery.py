fb5ae2cabe6a0a3c48bf201050c39ac1c42fcfb5
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-06 11:02:44 -0800

    edits

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,10 @@
 
 if __name__ == "__main__":
 
-     import hidden_4
+    import hidden_4
 
-     names = dir(hidden_4)
+    names = dir(hidden_4)
 
-     for name in names:
-         print(name)
+    for name in names:
+        if not name.startswith("__"):
+            print(name)
