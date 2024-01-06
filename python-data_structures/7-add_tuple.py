e5a04be3752020db4293829c85a16d19ae7d933c
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-06 13:31:23 -0800

    edits

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,8 +2,8 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    a = tuple_a (0, 0)
-    b = tuple_b (0, 0)
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
 
     first = a[0] + b[0]
     second = a[1] + b[1]
