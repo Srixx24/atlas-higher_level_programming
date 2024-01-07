1cee70c5defbfe2bf9cde53b957990b3e8ff633d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-06 21:20:14 -0800

    edits

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,8 +2,8 @@
 
 def delete_at(my_list=[], idx=0):
 
-    if idx < 0
-        return my_list:
+    if idx < 0:
+        return my_list
     elif idx >= len(my_list):
         return my_list
     del my_list[idx]
