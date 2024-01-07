8604580b06e8fb5a9ba1994db492756a995e227c
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-07 07:32:36 -0800

    edits

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 def search_replace(my_list, search, replace):
 
     new = list(my_list)
-    for i in range (len(new))
+    for i in range (len(new)):
         if new[i] == search:
             new[i] = replace
     return new
