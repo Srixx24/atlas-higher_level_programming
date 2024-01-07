7ac618142af1c83864715fc5da293f5181f59a97
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-07 08:06:23 -0800

    edits

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(a_dictionary):
 
     sorted_keys = sorted(a_dictionary.keys())
     for key in sorted_keys:
-        print(key, ":", a_dictionary[key])
+        print(f "{key}: {a_dictionary[key]}")
