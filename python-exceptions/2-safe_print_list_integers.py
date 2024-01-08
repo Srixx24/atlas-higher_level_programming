9f2b42756b638b16f99d50d3c64b2a807c627b6e
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-08 11:41:58 -0800

    edits

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(x):
             if isinstance(my_list[i], int):
                 print("{:d})".format(my_list[i]), end=" ")
-                count +=
+                count += 1
     except IndexError:
         pass
 
