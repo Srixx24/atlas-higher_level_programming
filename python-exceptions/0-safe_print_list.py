e89aefebaa221824902f94df3871713425f1cd20
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-08 11:17:44 -0800

    edits

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end=" ")
             count += 1
-        except:
-            return False
+        except IndexError:
+            break
     return 
