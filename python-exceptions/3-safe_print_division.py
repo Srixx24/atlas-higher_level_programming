bab01caab5f6fa55634c14518b7d5e881a076f9f
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-08 12:05:40 -0800

    edits

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 
+def safe_print_division(a, b):
 
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return none
+    finally:
+        print("Inside result: {}".format("{:.2f}".format(result))
+    return result
