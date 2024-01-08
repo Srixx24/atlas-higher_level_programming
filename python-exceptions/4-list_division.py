8f2a71f95a4bfe14fed9499c0d33325fe88f9835
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-08 13:36:51 -0800

    edits

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -21,4 +21,5 @@ def list_division(my_list_1, my_list_2, list_length):
                     print("out of range")
                     div = 0
                 finally:
-                    return results
+                    results.append(div)
+    return results
