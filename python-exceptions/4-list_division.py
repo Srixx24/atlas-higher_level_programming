9d2708fcdebd4e0cac3652abd8b688e6c46fcde6
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-08 13:49:52 -0800

    edits

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,18 +8,19 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             element_1 = my_list_1[i]
             element_2 = my_list_2[i]
-            if isinstance(element_1, (int, float)) and isinstance(element_2, (int, float)):
-                try:
-                    div = element_1 / element_2
-                except ZeroDivisionError:
-                    print("division by 0")
-                    div = 0
-                except TypeError:
-                    print("wrong type")
-                    div = 0
-                except IndexError:
-                    print("out of range")
-                    div = 0
-                finally:
-                    results.append(div)
+            if not isinstance(element_1, (int, float)) or not isinstance(element_2, (int, float)):
+                raise TypeError('wrong type')
+            try:
+                div = element_1 / element_2
+            except ZeroDivisionError:
+                print("division by 0")                    
+                div = 0
+            except IndexError:
+                print("out of range")
+                div = 0
+            finally:
+                results.append(div)
+        except TypeError:
+            print('wrong type')
+            results.append(0)
     return results
