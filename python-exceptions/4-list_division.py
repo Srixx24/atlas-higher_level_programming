01ce69f8101645cac8e7ba31848522593460e13b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-08 12:32:42 -0800

    edits

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
 
+def list_division(my_list_1, my_list_2, list_length):
 
+    results = []
+
+    for i in range(list_lengh):
+        try:
+            element_1 = my_list_1[i]
+            element_2 = my_list_2[i]
+            if isinstance(element_1, (int, float)) and isinstance(element_2, (int, float)):
+                try:
+                    div = my_list_1[i] / my_list_2[i]
+                except ZeroDivisionError:
+                    print("division by 0")
+                    div = 0
+                except TypeError:
+                    print("wrong type")
+                    div = 0
+                except IndexError:
+                    print("out of range")
+                    div = 0
+        finally:
+            return results
