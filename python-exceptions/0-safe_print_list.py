4448d6dca818af227ba736121ed4a6978b4cfd74
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-09 07:20:20 -0800

    edits

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@
 def safe_print_list(my_list=[], x=0):
 
     count = 0
-    for i in range(x):
-        try:
+    try:
+        for i in range(x):
             print(my_list[i], end=" ")
             count += 1
-        except IndexError:
+    except IndexError:
             break
     return 
