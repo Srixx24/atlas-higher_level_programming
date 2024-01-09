d4c61a7495fffc1192cddd01a41bd6fbaf0e410d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-09 07:54:11 -0800

    edits

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
                 print(my_list[i], end=" ")
                 count += 1
             except IndexError:
-                pass
+                break
     except TypeError:
         pass
 
