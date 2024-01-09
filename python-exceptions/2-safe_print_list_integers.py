082964afd6dc31bfa838e712cc1774c56f10795e
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-09 08:01:37 -0800

    edits

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,10 @@
 def safe_print_list_integers(my_list=[], x=0):
 
     count = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end=" ")
-                count += 1
-    except IndexError:
-        pass
-
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end=" ")
+            count += 1
+        except (ValueError, TypeError):
+            pass
     return count
