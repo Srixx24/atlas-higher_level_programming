390c9c158137391d828c8dd5b6caa8522276a32a
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-13 18:42:16 -0800

    edits

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""
+Class square that defines a square based on 0-square.py
+"""
 
+class Square:
+    """
+    A class representing a square
+    """
+    def _init_(self, size):
+        """
+        Starts new instance of the square class
 
+        Args:
+            size (int): size of square
+        """
+        self._size = size
