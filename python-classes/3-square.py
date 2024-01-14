1da4a4a70511667df4842ef18a42e400fd5a7233
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-13 19:23:26 -0800

    edits

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -24,8 +24,8 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            """
+    def area(self):
+        """
             returns the current square area
-            """
-            return self.__size ** 2
+        """
+        return self.__size ** 2
