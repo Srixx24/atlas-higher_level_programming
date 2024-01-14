61da1b9b3a168ed74eb0f8acc87528e12fd4e2c8
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-13 19:12:08 -0800

    edits

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,3 +1,31 @@
 #!/usr/bin/python3
+"""
+Class square
+"""
 
 
+class Square:
+    """
+    A class representing a square.
+    """
+    def __init__(self, size=0):
+        """
+        Starts new instance of the square class
+
+        Args:
+             size (int, optional): size of square, 0
+                raises:
+                TypeError: If size is not an integer.
+                ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """
+            returns the current square area
+            """
+            return self.__size ** 2
