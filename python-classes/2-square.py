1c04cc33ee0803653dcb870bfae8dda2557f920d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-13 19:01:01 -0800

    edits

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python3
+"""
+Class Square
+"""
 
 
+class Square:
+    """
+    A class representing a square.
+    """
+    def __int__(self, size=0):
+        """
+        Starts new instance of the square class
+
+        Args:
+            size (int, optional): size of square, 0
+                raises:
+                TypeError: If size is not an integer.
+                ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
