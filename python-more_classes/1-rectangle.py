d3cc8946fe67f94a293863786242111631bbfdad
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-15 11:55:24 -0800

    edits

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,3 +1,47 @@
 #!/usr/bin/python3
+"""
+Class Rectangle that defines a rectangle based on 0-rectangle.py
+"""
 
 
+class Rectangle:
+    """
+    Class Rectangle that defines a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int, optional): Width of rectangle
+            height (int, optional): Height of rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getting width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setting width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def height(self):
+        """Getting height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setting height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
