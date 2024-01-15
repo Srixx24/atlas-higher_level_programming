0e4d737f32c3814e89ef9ecef637231eb2d1bdbb
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-15 12:03:23 -0800

    edits

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -42,6 +42,7 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
     def height(self):
         """Getting height"""
         return self.__height
