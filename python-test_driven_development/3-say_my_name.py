17af711eba31c112859fbc5e6df141bdaae3c74e
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-15 08:07:54 -0800

    edits

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 """
-
+Function that prints My name is <first name> <last name>
 """
 
 
 def say_my_name(first_name, last_name=""):
     """
-
+    Function that prints My name is <first name> <last name>
 
     Args:
+        first_name (str): The first name
+        last_name (str, optional): The last name
 
     """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if last_name:
+        print("My name is {} {}".format(first_name, last_name))
