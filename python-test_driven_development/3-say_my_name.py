b9aa3cc298fc02f5e8d01812f2706b0af7b6d3e2
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-15 08:09:10 -0800

    edits

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -21,3 +21,5 @@ def say_my_name(first_name, last_name=""):
 
     if last_name:
         print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
