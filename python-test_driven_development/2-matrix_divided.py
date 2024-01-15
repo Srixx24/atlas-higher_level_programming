83bed7f4f0bc2ac34f16b9b80728c4240a548bb4
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-14 22:45:06 -0800

    edits

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,7 +4,6 @@ Divides all elements of a matrix by a given divisor
 """
 
 
-
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix by a given divisor
@@ -17,16 +16,15 @@ def matrix_divided(matrix, div):
         list: A new matrix
 
     """
-    if not all(isinstance(row, list) and all(isinstance(val, (int, float)) 
-    for val in row) for row in matrix):
+    if not all(isinstance(row, list) and all(isinstance(val, (int, float)) for val in row) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-    
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
