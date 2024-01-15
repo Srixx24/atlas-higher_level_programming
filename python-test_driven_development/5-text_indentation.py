0dab29a5d8885157cbd6a16b1ad20754c97e50c5
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-15 09:43:12 -0800

    edits

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 """
-
+function that prints a text with 2 new lines after each of these characters: ., ? and :
 """
 
 
 def text_indentation(text):
     """
-
+    function that prints a text with 2 new lines after each of these characters: ., ? and :
 
     Args:
+        text (str): Input
 
     """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    result = ""
+    for char in text:
+        result += char
+        if char in ['.', ',', '?', ':']:
+            result += "\n\n"
+
+    print(result.strip())
