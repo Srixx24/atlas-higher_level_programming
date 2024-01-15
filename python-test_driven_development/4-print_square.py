10612ccbbc126dae1675f1de2cf1ba7a89d4908e
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-15 08:56:39 -0800

    edits

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 """
-
+Prints a square with the character #
 """
 
 
 def print_square(size):
     """
-
+    Prints a square with the character #
 
     Args:
+        size (int): length of the square
 
     """
+    if not isinstance(size, (int, float)):
+        raise TypeError("size must be an integer")
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
