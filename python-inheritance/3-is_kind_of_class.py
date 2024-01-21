d0db2a7973fb6d6eaf562d03da017b3bfa65b90a
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-21 07:43:01 -0800

    edit

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-
+Function that returns True if the object is an instance of, or
+if the object is an instance of a class that inherited from,
+the specified class, otherwise False
 """
 
 
 def is_kind_of_class(obj, a_class):
     """
-
+    Checking if objects type is a_class or a sub class
     """
-
+    return isinstance(obj, a_class)
