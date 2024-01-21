36eb8bbd516313fb04a59b8f111bfae1fdaaa049
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-21 09:02:56 -0800

    edit

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -4,8 +4,21 @@ class Square that inherits from Rectangle 9-rectangle.py and 10-square.py
 """
 
 
-class :
-    """
+Rectangle = __import__('9-rectangle').Rectangle
 
-    """
 
+class Square(Rectangle):
+    """
+    A class representing a square inheriting from Rectangle
+    """
+    def __init__(self, size):
+        """
+        Starts instance of a square
+        """
+        self.integer_validator("_size", size)
+        super().__init__(size, size)
+        self._size = size
+    
+    def __str__(self):
+        """String for square"""
+        return "[Square] {size}/{size}".format(size=self.__size)
