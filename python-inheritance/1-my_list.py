423f3d95327111060741c247475438537f8bb610
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-21 10:06:32 -0800

    edit

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
-
+class MyList that inherits from list
 """
 
 
-
-def print_sorted(self):
+class MyList(list):
     """
-
+    MyList class that inherits from list
     """
+    def print_sorted(self):
+        """
+        Prints sorted list
+        """
+        print(sorted(self))
