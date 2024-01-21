8bb79a41ebf08dd8b0ce532003addc913017e61e
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-20 16:09:23 -0800

    edits

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Function to read and print a file"""
+
 
 def read_file(filename=""):
+    """Print file contents"""
     with open(filename,"r") as istream:
         print (istream.read(), end ="")
