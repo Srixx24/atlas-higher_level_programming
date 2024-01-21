aeae86f204fb6e6c73342dac63033c7c9ac0df46
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-21 07:35:27 -0800

    first push

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-
+Function that returns True if the object is exactly
+an instance of the specified class ; otherwise False.
 """
 
 
 def is_same_class(obj, a_class):
     """
-
+    Check if object is of type a_class
     """
-
+    return type(obj) == a_class
