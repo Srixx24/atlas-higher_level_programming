e230a836df205c8372b48bfb07f6dc7124dd03f6
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-21 07:48:00 -0800

    edit

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-
+Function that returns True if the object is an instance of a
+class that inherited (directly or indirectly) from the
+specified class, otherwise False
 """
 
 
 def inherits_from(obj, a_class):
     """
-
+    Checking if objects type is a_class or a sub class
     """
-
+    return isinstance(obj, a_class) and type(obj) != a_class
