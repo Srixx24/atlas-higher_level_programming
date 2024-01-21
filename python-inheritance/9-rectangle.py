34bd1e2642a196c9a169a37bdf1655051c40dc4c
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-21 08:37:49 -0800

    edit

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
 """
-class Rectangle that inherits from BaseGeometry
+Class Rectangle that inherits from BaseGeometry
 7-base_geometry.py and 8-rectangle.py
 """
 
 
-class BaseGeometry:
-    """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    """
 
+class Rectangle(BaseGeometry):
+    """
+    A class representing a Rectangle inheriting from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        Starts instance of a rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """string for rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """area for rectangle"""
+        return self.__width * self.__height
