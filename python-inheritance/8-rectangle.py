fb062adcbf735bf2444df47e7d2982669061a7eb
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-21 08:19:40 -0800

    edit

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,7 +4,18 @@ class Rectangle that inherits from BaseGeometry 7-base_geometry.py
 """
 
 
-class BaseGeometry:
-    """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+class Rectangle(BaseGeometry):
     """
+    A class representing a Rectangle inheriting from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        Starts instance of a rectangle
+        """
+        self.integer_validator("_width", width)
+        self.integer_validator("_height", height)
+        self.width = width
+        self.height = height
+
