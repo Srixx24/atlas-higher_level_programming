a9306d3e7fdfcf1e35997d3feef4c5e7e92c6439
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-21 07:54:27 -0800

    edit

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -6,6 +6,8 @@ BaseGeometry based on 5-base_geometry.py
 
 class BaseGeometry:
     """
-
+    A class representing geometric objects
     """
-
+    def area(self):
+        """Area of a geometric object"""
+        raise Exception ("area() is not implemented")
