cc1c20994c14c329b12ab47e15feaf12a321b7b4
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-22 20:47:41 -0800

    edits

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -3,10 +3,12 @@
 Function that writes an Object to a text file, using
 a JSON representation
 """
+import json
 
 
 def save_to_json_file(my_obj, filename):
     """
-
+    Writes an Object using a JSON representation
     """
-
+    with open(filename, "w") as ostream:
+        return json.dump(my_obj, filename)
