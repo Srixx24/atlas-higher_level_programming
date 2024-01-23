6af2a061886043aff7dbcac6a74d1f727a2e3ed8
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-23 08:31:42 -0800

    edits

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -8,3 +8,29 @@ class Student:
     """
     Defines class Student
     """
+    def __init__(self, first_name, last_name, age):
+        """
+        Start of student instance
+
+        Args:
+            first_name: First name of student
+            last_name: Last name of student
+            age: Student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a representation of a Student
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in
+                    attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """Loads data for Student"""
+        self.__dict__.update(json)
