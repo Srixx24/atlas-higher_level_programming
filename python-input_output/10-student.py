65c981bec8de55e2a4a9441c97bed459be0f47c6
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-23 08:21:07 -0800

    edits

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -27,3 +27,5 @@ class Student:
         """
         if attrs is None:
             return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
