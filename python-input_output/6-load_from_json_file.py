db51fd7263f144ecc353d81b718cc8b626ac3327
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-22 20:58:54 -0800

    edits

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -2,10 +2,12 @@
 """
 Function that creates an Object from a “JSON file”
 """
+import json
 
 
 def load_from_json_file(filename):
     """
-
+    Create an object from a JSON file
     """
-
+    with open(filename, "r") as istream:
+        return json.load(istream)
