86b911a5054a681f9c11ead8d8ac48fc93040066
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-23 07:30:05 -0800

    edits

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,5 +6,6 @@ Function that returns the dictionary description with simple data structure
 
 def class_to_json(obj):
     """
-
+    Serialize the writable attributes of an object
     """
+    return obj._dict_
