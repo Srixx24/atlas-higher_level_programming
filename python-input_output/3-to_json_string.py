92c77daf3ea0012737f3ee4144c48637a51bc81c
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-22 20:35:14 -0800

    edits

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,10 +2,11 @@
 """
 Function that returns the JSON representation of an object
 """
+import json
 
 
 def to_json_string(my_obj):
     """
-
+    Returns the JSON representation
     """
-
+    return json.dumps(my_obj)
