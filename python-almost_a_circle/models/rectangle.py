5626ca95d406a9602d94672c2eda10dc38fe003a
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-27 08:21:56 -0800

    edits

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,6 +11,50 @@ class Rectangle(Base):
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Start of class Rectangle
+        Start of instance for class Rectangle
         """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
+    @property
+    def width(self):
+        """Getting width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setting width"""
+        self.__Width = value
+
+    @property
+    def height(self):
+        """Getting height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setting height"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getting x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value)
+        """Setting x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getting y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setting y"""
+        self.__y = value
