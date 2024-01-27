2e557b97edc9bc6203fb4932bb7d2fcc8ef94f9f
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-27 15:24:53 -0800

    edits

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(Base):
     """
     Class Rectangle
     """
-    def __init__(self, width, height, x=0, y=0, id):
+    def __init__(self, id, width, height, x=0, y=0):
         """
         Start of instance for class Rectangle
         """
