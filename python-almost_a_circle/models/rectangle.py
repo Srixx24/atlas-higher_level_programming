f4afaf13823d7951b249f3e8bec760bd63bf431d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-27 14:40:21 -0800

    edits

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,7 +21,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """String of rectangle"""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} \
+        - {self.width}/{self.height}"
 
     @property
     def width(self):
