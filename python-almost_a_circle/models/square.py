66d89ac50518a06fbaee8fc179f61c265d1f66b1
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-28 11:53:32 -0800

    edits

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -23,6 +23,18 @@ class Square(Rectangle):
                 self.id, self.x, self.y, self.size
             )
 
+    def update(self, *args, **kwargs):
+        """Attributes updates"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+
     @property
     def size(self):
         """Getting size"""
