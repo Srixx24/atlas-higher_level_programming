7b62d2bed3da18608dab96a5972149462c421331
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-01-28 12:52:55 -0800

    edits

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,7 +24,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation"""
-        if list_dictionaries is None or \
-                len(list_dictionaries) == 0:
+        if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
