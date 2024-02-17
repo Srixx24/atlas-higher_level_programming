f254ebca16a936af4adaa263aabf17d696d8622a
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-17 10:50:58 -0800

    Edits

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `second_table` (
 );
 -- Records to be inserted
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES
-(1, 'John', 10)
-(2, 'Alex', 3)
-(3, 'Bob', 14)
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
 (4, 'George', 8);
