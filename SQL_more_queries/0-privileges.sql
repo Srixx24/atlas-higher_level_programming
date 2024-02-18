6a17c28203d53f474563bc938abe7986ec5ac89f
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-18 08:45:57 -0800

    Edits

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- List all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANT FOR 'user_0d_1'@'localhost';
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+-- List all privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- List all privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
