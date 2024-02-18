c6134769d377906c9105961466ac129b407fd3c0
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-18 08:51:21 -0800

    Edits

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- List all privileges for user_0d_1
+-- Lists all privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- List all privileges for user_0d_2
+-- Lists all privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
