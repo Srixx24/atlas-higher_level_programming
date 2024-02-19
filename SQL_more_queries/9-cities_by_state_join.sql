8b73c89afb5ccf6064f0ce51c2371d74152deddb
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-18 16:20:54 -0800

    Edits

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 FROM states JOIN cities
-WHERE cities.state_id = state_id
+WHERE cities.state_id = state.id
 ORDER BY cities.id ASC;
