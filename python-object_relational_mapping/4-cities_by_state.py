169b60bbe06c06ef9fa7d3673bff4cd0ba811f95
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-20 09:47:39 -0800

    Edits

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -2,4 +2,41 @@
 """
 Lists all cities from the database hbtn_0e_4_usa
 """
+import MySQLdb
+import sys
 
+
+def list_cities(username, password, database):
+    try:
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=database
+                )
+
+        cursor = db.cursor()
+
+        command = "SELECT * FROM cities ORDER BY id ASC"
+        cursor.execute(command)
+
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL: {}".format(e))
+
+    finally:
+        if db:
+            db.close()
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_cities(username, password, database)
