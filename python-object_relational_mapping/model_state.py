fc9273ffeb54e62af8128f170ccacd6647d9d279
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-20 11:08:56 -0800

    Edits

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,3 +2,28 @@
 """
 contains the class definition of a State and an instance Base = declarative_base()
 """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Start of class State instance
+    """
+    __tablename__ = 'states'
+
+    id = Column(
+            Integer,
+            autoincrement=True,
+            unique=True,
+            nullable=False,
+            primary_key=True
+            )
+
+    name = Column(
+            String(128),
+            nullable=False,
+            )
