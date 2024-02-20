f87fed0c3c89f5d07222c213b532faee86eaa903
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-20 12:49:08 -0800

    Edits

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -2,4 +2,24 @@
 """
 adds the State object “Louisiana” to the database hbtn_0e_6_usa
 """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louisiana = State (name="Louisiana")
+
+    session.add(louisiana)
+
+    session.commit()
+
+    print(louisiana.id)
+
+    session.close()
