36fe49459828ebb012fd987bf5d2e2e93206f822
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-20 11:40:07 -0800

    Edits

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 """
-lists all State objects from the database hbtn_0e_6_usa
+Lists all State objects from the database hbtn_0e_6_usa
 """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for instance in Session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
