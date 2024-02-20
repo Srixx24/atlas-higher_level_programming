255fb7d6c6fc925eb8f287eec3ccdd980217b1dc
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-20 12:12:40 -0800

    Edits

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,4 +2,20 @@
 """
 lists all State objects that contain the letter a from the database hbtn_0e_6_usa
 """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id).filter(
+            State.name.like('%a%')):
+        print("{}: {}".format(instance.id, instance.name))
+
+    session.close()
