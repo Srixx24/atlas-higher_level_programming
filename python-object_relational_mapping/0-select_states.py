1703f2c846493a71db9602ce972ce6976535b8c1
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-20 08:13:04 -0800

    Edits

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,3 +2,28 @@
 """
 Lists all states from the database hbtn_0e_0_usa
 """
+import MySQLdb
+
+def list_states(username, password, database):
+
+    if __name__ == '__main__':
+        try:
+            db = MySQLdb.connect(host="localhost", 
+                    user=username,
+                    passwd=password,
+                    db=database)
+
+            cursor = db.cursor()
+
+            cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+            rows = cursor.fetchall()
+
+            for row in rows:
+                print (row)
+
+        except MySQLdb.Error as e:
+            print("Error connecting to MySQL: {}" .format(e))
+        finally:
+            if db:
+                db.close()
