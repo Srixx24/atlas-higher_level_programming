9e4b381c02b2250372cc435e69c2350dec168367
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-20 08:47:07 -0800

    Edits

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,15 +8,17 @@ import sys
 
 def list_states(username, password, database):
     try:
-        db = MySQLdb.connect(host="localhost",
-                            port=3306,
-                            user=username,
-                            passwd=password,
-                            db=database)
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=database
+                )
 
         cursor = db.cursor()
 
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'n%' ORDER BY id ASC")
 
         rows = cursor.fetchall()
 
