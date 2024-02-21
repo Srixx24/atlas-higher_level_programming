c3b3613fc5907afd793d0c21aef155939d3f7cf6
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-20 17:35:16 -0800

    Edits

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,3 +2,35 @@
 """
 Contains the class definition of a City.
 """
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    Start of class City instance
+    """
+    __tablename__ = 'cities'
+
+    id = Column(
+            Integer,
+            autoincrement=True,
+            unique=True,
+            nullable=False,
+            primary_key=True
+            )
+
+    name = Column(
+            String(128),
+            nullable=False,
+            )
+
+    state_id = Column(
+            Integer,
+            nullable=False,
+            ForeignKey('states.id')
+            )
