b9b32b1cc5dcdeb9b29f20f8b9ce023bae0bb9db
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-23 11:55:38 -0500

    Edits

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arrLines = [ 'C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let x of arrLines){
-    console.log(x)
+const arrLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const x of arrLines) {
+  console.log(x);
 }
