21df65346db770ad939d8becad54dbfad91a9690
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-23 10:37:35 -0500

    Edits

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const argsVar = process.argv.length - 2;
+if (argsVar == 0) {
+    console.log("No argument");
+  } else if (argsVar== 1) {
+    console.log("Argument found");
+  } else {
+    console.log("Arguments found");
+  }
