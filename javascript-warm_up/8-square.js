0ebc9c8a93edbfdf5cb7fbe298f0812f949cda3b
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-23 13:48:56 -0500

    Edits

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const square = process.argv[2];
+if (isNaN(parseInt(square))) {
+  console.log('Missing size');
+} else {
+  for (let x = 0; square > x; ++x) {
+    console.log('x'.repeat(square));
+  }
+}
