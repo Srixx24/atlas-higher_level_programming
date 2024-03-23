d6d7fb0a288476e919d8fd0809397e37f27485d9
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-23 13:17:23 -0500

    Edits

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const theJoke = 'C is fun';
+const numb = parseInt(process.argv[2]);
+if (isNaN(numb) === true) {
+  console.log('Missing number of occurrences');
+} else {
+  for (const x = 0; x < numb; x++) {
+    console.log(theJoke);
+  }
+}
