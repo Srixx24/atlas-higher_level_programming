491e99bc67960a4cfb4b662a367fc37838a89b0c
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-23 21:50:33 -0500

    Edits

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 function factorial(x) {
   if (isNaN(x)) {
-    return 1;
-  } else if (x > 0) {
-    return factorial(x - 1);
-  } else {
   return 1;
-  }
+} else if (x > 0) {
+  return factorial(x - 1);
+} else {
+  return 1;
+}
 }
 
 const intValue = parseInt(process.argv[2]);
