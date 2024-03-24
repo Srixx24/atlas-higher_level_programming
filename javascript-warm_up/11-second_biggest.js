accebc2fc1e7ec5e4d5ac60908dbe3ea4363f199
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-23 22:32:24 -0500

    Edits

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-if (process.argv[3] == undefined) {
+if (process.argv[3] === undefined) {
   console.log(0);
-}else {
+} else {
   const numb = process.argv.slice(2);
-  numb.sort(( a, b) => b - a);
-  const secnBig = numb[1]
+  numb.sort((a, b) => b - a);
+  const secnBig = numb[1];
   console.log(secnBig);
 }
