bf5acee68476cfafad29fa982a986337b285e035
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-25 00:51:02 -0500

    Edits

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w < 1 || h < 1 || w === undefined || h === undefined) {
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  print () {
+    for (let x = 0; x < this.height; ++x) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
