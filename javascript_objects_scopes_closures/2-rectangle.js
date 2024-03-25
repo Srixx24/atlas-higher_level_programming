d8c59a363e1682d8e57639076e6b4d3292309c97
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-25 00:00:32 -0500

    Edits

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,9 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w < 1 || h < 1 || w === undefined || h === undefined) {
-      this.width = w;
-      this.height = h; 
     } else {
-      this.width = 0;
-      this.height = 0;
+      this.width = w;
+      this.height = h;
     }
   }
 };
