9624956ae3eadf75ad249ecd25dbcd118bcf735b
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-26 13:16:13 -0500

    Edits

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const R = list.lenght - 1;
+  const L = 0;
+
+  while (R > L) {
+    const temp = list[L];
+    list[L] = list[R];
+    list[R] = temp;
+    L++;
+    R--;
+  }
+  return list;
+};
