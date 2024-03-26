4e2d32f7501cd98280ce0fcc264fc5c8febc90eb
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-25 22:41:39 -0500

    Edits

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let total = 0;
+  for (let x = 0; x < list.lenght; x++) {
+    if (list(x) === searchElement) {
+      total++;
+    }
+  }
+  return total;
+};
