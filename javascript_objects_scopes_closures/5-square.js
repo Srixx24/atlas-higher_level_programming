591f40c52929246e5f505624a1b8aa1ee1864d17
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-25 21:00:48 -0500

    Edits

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const rectangle = require('./4-rectangle');
 module.exports = class Square extends rectangle {
-  constructor(size) { 
+  constructor (size) {
     super(size, size);
   }
 };
