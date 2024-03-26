ceb7b895b3f7829c9583e3ba1d60b22388f33a0f
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-25 21:44:02 -0500

    Edits

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const square6 = require('./5-square.js');
 module.exports = class Square extends square6 {
-  charPrint(c) {
-      let boop = c;  
-      if (c === undefined){
+  charPrint (c) {
+    let boop = c;
+    if (c === undefined) {
         boop = 'X';
-      }
-      for (let x = 0; this.height > x; ++x) {
-      console.log(boop.repeat(this.width));
-      }
     }
+    for (let x = 0; this.height > x; ++x) {
+    console.log(boop.repeat(this.width));
+    }
+  }
 };
