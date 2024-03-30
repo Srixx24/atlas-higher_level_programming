85cb931602c38aac8c4b20a258144279caca73de
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-29 21:20:59 -0500

    Edits

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi.dev/api/people/18/'
+request(url, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const parsResp = JSON.parse(body);
+      console.log(parsResp.films.length);
+    }
+  });
