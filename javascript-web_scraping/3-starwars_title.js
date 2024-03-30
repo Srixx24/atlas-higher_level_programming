c3b3077f064eb19fcd20c89c2bafceff59eb7e40
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-29 21:11:24 -0500

    Edits

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,10 +3,10 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 request(url, (error, response, body) => {
-  if (error){
+  if (error) {
     console.log(error);
   } else {
     const parsResp = JSON.parse(body);
-  console.log(parsResp.title);
- }
+    console.log(parsResp.title);
+  }
 });
