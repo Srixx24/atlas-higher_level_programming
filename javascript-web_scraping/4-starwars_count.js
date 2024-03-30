c6cb63fd2f32214b86abe6fe4c611a035be697da
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-29 21:53:34 -0500

    Edits

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,13 +3,13 @@ const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/`;
 const charID = 18;
 request(url, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const parsResp = JSON.parse(body);
-      const filtered = parsResp.results.filter(film => 
-        film.characters.includes(`https://swapi-api.hbtn.io/api/people/${charID}`)
+  if (error) {
+    console.log(error);
+  } else {
+    const parsResp = JSON.parse(body);
+    const filtered = parsResp.results.filter(film =>
+      film.characters.includes(`https://swapi-api.hbtn.io/api/people/${charID}/`)
         );
-        console.log(filtered.length)
-    }
+    console.log(filtered.length);
+  }
 });
