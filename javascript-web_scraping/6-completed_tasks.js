83626b76a6516225d5d580474d3df7c93d5afb62
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-29 22:44:50 -0500

    Edits

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const url = `https://jsonplaceholder.typicode.com/todos`
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const parseResp = JSON.parse(body);
+    const completed = {};
+    parseResp.forEach((todo) => {
+      if (todo.completed === true) {
+        if (completed[todo.userId] === undefined){
+          completed[todo.userId] = 1;
+        } else {
+          completed[todo.userId]++;
+        } 
+      }
+    });
+  }
+});
