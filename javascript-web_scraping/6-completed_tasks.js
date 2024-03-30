5190edd66b3e80a169c1e3dab7b74f61ad541498
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-29 22:51:02 -0500

    Edits

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = `https://jsonplaceholder.typicode.com/todos`
+const url = `https://jsonplaceholder.typicode.com/todos`;
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
