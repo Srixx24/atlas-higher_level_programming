7e39a500593ee0a952da6cbad7de26fa887a43ce
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-29 22:06:42 -0500

    Edits

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const fs = require('fs');
+    fs.writeFile(process.argv[3], body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
