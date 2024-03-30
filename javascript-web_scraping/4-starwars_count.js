b60f3d8f8497d362b1092d3f749f82dd7af3c05e
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-29 21:34:31 -0500

    Edits

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/`;
+const charID = 18;
 request(url, (error, response, body) => {
     if (error) {
       console.log(error);
     } else {
       const parsResp = JSON.parse(body);
-      console.log(parsResp.films.length);
+      console.log(parsResp.films.[charID]);
     }
 });
