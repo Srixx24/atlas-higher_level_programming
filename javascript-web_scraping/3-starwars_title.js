70e6d67f401f7ebd809b8ac3bd7c1ffe1e027b95
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-29 21:10:27 -0500

    Edits

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const movieId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/${movieId}'
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 request(url, (error, response, body) => {
   if (error){
     console.log(error);
