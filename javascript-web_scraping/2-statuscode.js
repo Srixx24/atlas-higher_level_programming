f1ec9e910f00fb9714ce8e2849cf9c91b6fd4a5b
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-29 20:33:41 -0500

    Edits

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (error, respo) => {
+  if (respo) {
+    console.log(`code: ${respo.statusCode}`);
+} else if (error) {
+    console.log(error);
+}
+});
