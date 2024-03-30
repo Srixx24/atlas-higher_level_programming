7063a1f8c6ffb8d46d97b600e006eea16d8188df
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-29 21:22:29 -0500

    Edits

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.dev/api/people/18/';
+const url = `https://swapi.dev/api/people/18/`;
 request(url, (error, response, body) => {
     if (error) {
       console.log(error);
